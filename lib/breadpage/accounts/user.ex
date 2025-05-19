e4866519a675d2e7ca2707bd0fa9449ea098e7e3
Mark-Gallacher <mark.g.gallacher@gmail.com>
2025-05-19 17:40:21 +0100

    add name field to user scheme

--- lib/breadpage/accounts/user.ex
+++ lib/breadpage/accounts/user.ex
@@ -4,6 +4,7 @@ defmodule Breadpage.Accounts.User do
 
   schema "users" do
     field :email, :string
+    field :name, :string
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
