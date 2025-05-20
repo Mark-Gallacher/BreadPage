1ab540e4c585825d631dbaf245e01a03f1cf0c45
Mark-Gallacher <mark.g.gallacher@gmail.com>
2025-05-20 20:47:30 +0100

    add post schema to liveview

---
+++ lib/breadpage/articles/post.ex
@@ -0,0 +1,24 @@
+defmodule Breadpage.Articles.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :id, autogenerate: true}
+  schema "posts" do
+    field :title, :string
+    field :description, :string
+    field :active, :boolean
+    field :tags, {:array, :string}
+    timestamps(type: :utc_datetime)
+
+    belongs_to :user, Breadpage.Accounts.User
+    belongs_to :recipe, Breadpage.Articles.Recipe
+  end
+
+  defp changeset(post, attrs) do
+    post
+    |> cast(attrs, [:title, :description, :tags, :user_id, :recipe_id])
+    |> validate_required([:title, :user_id, :recipe_id])
+    |> validate_length(:title, min: 8, max: 120)
+    |> validate_length(:description, max: 500)
+  end
+end
