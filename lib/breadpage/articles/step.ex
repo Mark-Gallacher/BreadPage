3419a829c3d86b0fb27597a498d28e0027829c95
Mark-Gallacher <mark.g.gallacher@gmail.com>
2025-05-26 21:25:31 +0100

    add database constrainsts to steps changeset - also make changeset/2 a public function

--- lib/breadpage/articles/step.ex
+++ lib/breadpage/articles/step.ex
@@ -17,7 +17,7 @@ defmodule Breadpage.Articles.Step do
     timestamps(type: :utc_datetime)
   end
 
-  defp changeset(step, attrs) do
+  def changeset(step, attrs) do
     step
     |> cast(attrs, [
       :step_number,
@@ -35,29 +35,48 @@ defmodule Breadpage.Articles.Step do
     |> validate_comment()
     |> validate_temperature()
     |> validate_type()
+    |> validate_unit()
   end
 
   defp validate_step(changeset) do
     changeset
     |> validate_required(:step_number)
-    |> validate_number(:step_number, greater_than_or_equal: 0)
+    |> validate_number(:step_number, greater_than_or_equal_to: 0)
     |> validate_number(:step_number, less_than: 10000)
   end
 
   defp validate_ingredient(changeset) do
     changeset
     |> validate_length(:ingredient, max: 100)
+    |> check_constraint(:ingredient,
+      name: :if_ingredient_then_quantity_not_null,
+      message: "Cannot specify an ingredient without also specifying a quantity"
+    )
   end
 
   defp validate_quantity(changeset) do
     changeset
-    |> validate_number(:quantity, greater_than_or_equal: 0)
+    |> validate_number(:quantity, greater_than_or_equal_to: 0)
     |> validate_number(:quantity, less_than: 10000)
+
+    |> check_constraint(:quantity,
+      name: :if_quantity_then_quatity_is_positive,
+      message: "Quantity cannot be a negative number"
+    )
   end
 
   defp validate_comment(changeset) do
     changeset
     |> validate_length(:comment, max: 500)
+  end
+
+  defp validate_unit(changeset) do
+    changeset
+    |> validate_length(:unit, max: 500)    
+    |> check_constraint(:unit,
+      name: :if_quantity_then_unit_not_null,
+      message: "Cannot specify a quantity without also specifying a unit"
+    )
   end
 
   defp validate_type(changeset) do
@@ -68,6 +87,10 @@ defmodule Breadpage.Articles.Step do
   defp validate_temperature(changeset) do
     changeset
     |> validate_number(:temperature, less_than: 1000)
-    |> validate_number(:temperature, greater_than: -100)
+    |> validate_number(:temperature, greater_than: 0)
+    |> check_constraint(:temperature,
+      name: :if_temperature_then_temperature_is_positive,
+      message: "Temperature cannot be a negative number"
+    )
   end
 end
