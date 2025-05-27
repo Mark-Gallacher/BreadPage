2ca85f52cc2ccb5e375afd9c9ed95900b2a49ace
Mark-Gallacher <mark.g.gallacher@gmail.com>
2025-05-27 21:23:25 +0100

    add fixture for recipes - without associations [wip]

--- test/support/fixtures/articles_fixtures.ex
+++ test/support/fixtures/articles_fixtures.ex
@@ -1,9 +1,11 @@
 defmodule Breadpage.ArticlesFixtures do
+  alias Breadpage.AccountsFixtures
+
   def valid_step_attributes(attrs \\ %{}) do
     Enum.into(attrs, %{
       step_number: 1,
       ingredient: "strong white flour",
-      quantity: Decimal.new(1000),
+      quantity: Decimal.new("1000"),
       unit: "grams",
       temperature: 25,
       type: "flour",
@@ -12,8 +14,17 @@ defmodule Breadpage.ArticlesFixtures do
     })
   end
 
-  # def valid_recipe_attributes(attrs) do
-  # end
+  def valid_recipe_attributes(attrs \\ %{}) do
+    Enum.into(attrs, %{
+      hydration: Decimal.new("0.7"),
+      prep_time: Duration.new!(hour: 5, minute: 30),
+      bake_time: Duration.new!(hour: 4, minute: 5),
+      source: "self"
+    })
+  end
+
+  defp add_recipe_associations() do
+  end
 
   # def valid_post_attributes do
   # end
@@ -25,5 +36,14 @@ defmodule Breadpage.ArticlesFixtures do
       |> Breadpage.Articles.create_step()
 
     step
+  end
+
+  def recipe_fixtures(attrs \\ %{}) do
+    {:ok, recipe} =
+      attrs
+      |> valid_recipe_attributes()
+      |> Breadpage.Articles.create_recipe()
+
+    recipe
   end
 end
