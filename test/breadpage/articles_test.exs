f4ad1328fcfee1d836866de29d8b4edca7906001
Mark-Gallacher <mark.g.gallacher@gmail.com>
2025-05-27 22:01:05 +0100

    add tests to cover invalid and different valid cases of the step schema

--- test/breadpage/articles_test.exs
+++ test/breadpage/articles_test.exs
@@ -19,6 +19,91 @@ defmodule Breadpage.ArticlesTest do
       assert step.comment == "High Protein Flour is ideal!"
       assert step.time == Duration.new!(hour: 5, minute: 30)
     end
+
+    test "create_step/2 with valid missing data" do
+      valid_step = %{step_number: 1}
+      assert {:ok, %Step{} = step} = Articles.create_step(valid_step)
+
+      assert step.step_number == 1
+      assert step.ingredient == nil
+      assert step.quantity == nil
+      assert step.unit == nil
+      assert step.temperature == nil
+      assert step.type == nil
+      assert step.comment == nil
+      assert step.time == nil
+    end
+
+    test "create_step/2 with invalid step_number" do
+      valid_step = Breadpage.ArticlesFixtures.valid_step_attributes()
+
+      assert {:error, %Ecto.Changeset{} = changeset} =
+               Articles.create_step(Map.delete(valid_step, :step_number))
+
+      assert changeset.valid? == false
+      assert %{step_number: ["can't be blank"]} = errors_on(changeset)
+    end
+
+    test "create_step/2 with missing quantity" do
+      valid_step = Breadpage.ArticlesFixtures.valid_step_attributes()
+
+      assert {:error, %Ecto.Changeset{} = changeset} =
+               Articles.create_step(Map.delete(valid_step, :quantity))
+
+      assert changeset.valid? == false
+
+      assert %{ingredient: ["Cannot specify an ingredient without also specifying a quantity"]} =
+               errors_on(changeset)
+    end
+
+    test "create_step/2 with missing units" do
+      valid_step = Breadpage.ArticlesFixtures.valid_step_attributes()
+
+      assert {:error, %Ecto.Changeset{} = changeset} =
+               Articles.create_step(Map.delete(valid_step, :unit))
+
+      assert changeset.valid? == false
+
+      assert %{unit: ["Cannot specify a quantity without also specifying a unit"]} =
+               errors_on(changeset)
+    end
+
+    test "create_step/2 with invalid temperature" do
+      invalid_step = Breadpage.ArticlesFixtures.valid_step_attributes(%{temperature: -1})
+
+      assert {:error, %Ecto.Changeset{} = changeset} =
+               Articles.create_step(invalid_step)
+
+      assert changeset.valid? == false
+
+      assert %{temperature: ["must be greater than 0"]} =
+               errors_on(changeset)
+    end
+
+    test "create_step/2 with invalid quantity" do
+      invalid_step = Breadpage.ArticlesFixtures.valid_step_attributes(%{quantity: -1})
+
+      assert {:error, %Ecto.Changeset{} = changeset} =
+               Articles.create_step(invalid_step)
+
+      assert changeset.valid? == false
+
+      assert %{quantity: ["must be greater than or equal to 0"]} =
+               errors_on(changeset)
+    end
+
+    test "create_step/2 with invalid time" do
+      invalid_step =
+        Breadpage.ArticlesFixtures.valid_step_attributes(%{time: Duration.new!(hour: -1)})
+
+      assert {:error, %Ecto.Changeset{} = changeset} =
+               Articles.create_step(invalid_step)
+
+      assert changeset.valid? == false
+
+      assert %{time: ["Cannot be negative"]} =
+               errors_on(changeset)
+    end
   end
 
   describe "recipes" do
